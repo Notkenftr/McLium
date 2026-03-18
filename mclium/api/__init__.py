e6d629e74ae564c8af92c488e7de3f8240a29e31
notkenftr <justkenftr@gmail.com>
2026-03-18 21:15:21 +0700

    add PacketBuilderWrappedAPi

--- mclium/api/__init__.py
+++ mclium/api/__init__.py
@@ -1,5 +1,5 @@
 from .subcommand.subcommand import SubCommandModule
 from .mclium_task.mcliumtask import McLiumTaskModule
-from .network.mc_protocol.packet import PacketBuilder, PacketFlow, PacketFieldType, _Field
+from .network.mc_protocol.packet import PacketBuilder, PacketFlow, PacketFieldType, _Field,PacketBuilderWrappedApi
 from .network.mc_protocol.packet_list import PacketList
 from .network.mc_session.protocol_session import ProtocolSession
--- mclium/api/network/mc_protocol/__init__.py
+++ mclium/api/network/mc_protocol/__init__.py
@@ -1,3 +1,4 @@
 from .encode import Encode as Encode
 from .read import Read as Read
 from .packet_list import PacketList
+from .packet import PacketBuilderWrappedApi
