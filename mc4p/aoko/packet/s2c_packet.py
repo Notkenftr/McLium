0cf8ce57139f508d82cb08949055a7581aa6281f
notkenftr <justkenftr@gmail.com>
2026-03-26 10:35:36 +0700

    update S2CPacket mc4p.aoko - rm _parser_packet_id

--- mc4p/aoko/packet/s2c_packet.py
+++ mc4p/aoko/packet/s2c_packet.py
@@ -3,14 +3,21 @@ from mc4p.aoko.entities.raw_packet import RawPacket
 from mc4p.aoko.utils.read import Read
 
 class S2CPacket(RawPacket):
+    """
+    This is an API that provides packet parsing from the server. This API only works when it's a single packet,
+    so if you have a byte string that's unclear whether it's a single packet or a multipacket, use AokoPacketStream to parse it first.
+    """
     def __init__(self,
                  packet_data: bytes,
+                 *,
+                 auto_parser:bool = True,
                  compressed: bool = False):
         super().__init__(packet_data)
         self.packet_data = packet_data
         self.compressed = compressed
 
         self.offset = 0
+        self.payload_offset = 0
         self.data_length_pos = 0
         self.payload_length_pos = 0
 
@@ -18,35 +25,30 @@ class S2CPacket(RawPacket):
         self.data_length = 0
 
         self.packet_id = 0
+        self.payload = b''
 
-        self._parser()
-        self._parser_packet_id()
+        if auto_parser:
+            self._parser()
 
     def reset_offset(self):
         self.offset = 0
 
     def _parser(self):
+        self.offset = 0
+        self.payload_offset = 0
+        self.packet_length, self.offset = Read.read_varint(self.packet_data, self.offset)
         if self.compressed:
-            self.packet_length,self.offset = Read.read_varint(self.packet_data,self.offset)
             self.data_length_pos = self.offset
-            self.data_length,self.offset = Read.read_varint(self.packet_data,self.offset)
-            self.reset_offset()
-        else:
-            self.packet_length,self.offset = Read.read_varint(self.packet_data,self.offset)
-            self.data_length_pos = self.offset
-            self.reset_offset()
-
-    def _parser_packet_id(self):
-        if self.compressed:
             if self.data_length == 0:
-                payload = self.packet_data[self.data_length_pos:]
-                self.packet_id,_ = Read.read_varint(payload,0)
+                self.payload_data = self.packet_data[self.offset:]
+                self.packet_id, self.payload_offset = Read.read_varint(self.payload_data, 0)
             else:
-                payload = self.packet_data[self.data_length_pos:]
-                decompressed_payload = zlib.decompress(payload)
-                self.packet_id,_ = Read.read_varint(decompressed_payload, 0)
+                compressed_payload = self.packet_data[self.offset: self.offset + self.packet_length]
+                self.payload_data = zlib.decompress(compressed_payload)
+                self.packet_id, self.payload_offset = Read.read_varint(self.payload_data, 0)
         else:
-            self.packet_id,_ = Read.read_varint(self.packet_data,self.data_length_pos)
+            self.payload_data = self.packet_data[self.offset:]
+            self.packet_id, self.payload_offset = Read.read_varint(self.payload_data, 0)
 
     def get_data_length(self) -> int:
         return self.data_length
@@ -61,5 +63,5 @@ class S2CPacket(RawPacket):
 
 if __name__ == '__main__':
     data = b'\x14\x00\x86\x06\rplay.2y2c.orgc\xdd\x02'
-    packet = S2CPacket(data, compressed=False)
+    packet = S2CPacket(data,auto_parser=True, compressed=False)
     print(packet.packet_id)
