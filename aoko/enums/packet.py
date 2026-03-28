a4f5261a065bc1fefc18d341ef126d18e36a5b99
notkenftr <justkenftr@gmail.com>
2026-03-28 21:14:12 +0700

    update folder struc, a aoko_sock packet

--- aoko/enums/packet.py
+++ aoko/enums/packet.py
@@ -1,7 +1,27 @@
 from enum import Enum
 
 class AokoPacketFieldType(Enum):
-    pass
+    VARINT = "varint"
+    STRING = "string"
+    INT = "int"
+    BOOL = "bool"
+    UUID = "uuid"
+    UNSIGNED_SHORT = "unsigned_short"
+    UNSIGNED_BYTE = "unsigned_byte"
+    LONG = "long"
+    BYTE = "byte"
+    FLOAT = "float"
+    DOUBLE = "double"
 
 class AokoPacketBuildMode(Enum):
-    pass
+    BYTEARRAY = "bytearray"
+    BYTEPLUS = "byteplus"
+
+class AokoPacketHooker(Enum):
+    HEAD = "head"
+    AFTER_ENCODE_FIELD = "after_encode_field"
+    TAIL = "tail"
+
+class AokoPacketRegister(Enum):
+    HOOKER = "hooker"
+
